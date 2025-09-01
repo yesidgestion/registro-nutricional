7599a9bb55d27dd39fc7e32e3bcac17b030b65b7
yesidgestion <yesidgestion@gmail.com>
2025-09-01 13:47:43 -0500

    actualizar get-user-data

--- netlify/functions/get-user-data.js
+++ netlify/functions/get-user-data.js
@@ -1,7 +1,7 @@
 export default async (req, context) => {
   // Ya no hay verificación de usuario. Se devuelve la lista a todo el mundo.
   const todasLasUnidades = [
-    "AMALINA", "KAISHAIPA", "MULAMANA", "OULECHI",
+    "amalina", "kaishaipa", "mulamana", "oulechi",
     "UNIDAD_5", "UNIDAD_6", "UNIDAD_7", "UNIDAD_8", "UNIDAD_9", "UNIDAD_10"
   ];
 
